8244e73631a0d8d1b5e1115c0b2424e8679a5145
Rahul Waghmare <76624779+rahul5522@users.noreply.github.com>
2023-03-06 10:04:00 +0530

    Debugging webhook

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -60,6 +60,8 @@ const createBooking = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
+  
+  console.log(tour,user,price)
 
   await Booking.create({ tour, user, price });
 };
